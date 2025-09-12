f557374feb3ad5ee313d6122414c26c9eb75959e
Adesina08 <153903110+Adesina08@users.noreply.github.com>
2025-09-12 23:50:56 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
             <ul className="space-y-3 text-sm opacity-90">
               <li><a href="/plans" className="hover:text-ibuild-red">Plan & Pricing</a></li>
               <li><a href="/tools" className="hover:text-ibuild-red">Modules</a></li>
-              <li><a href="#communication" className="hover:text-ibuild-red">Communication Features</a></li>
+              <li><a href="/tools" className="hover:text-ibuild-red">Communication Features</a></li>
             </ul>
           </div>
 
@@ -42,7 +42,7 @@ const Footer = () => {
           <div>
             <h5 className="text-lg font-semibold mb-4 opacity-90">Support</h5>
             <ul className="space-y-3 text-sm opacity-90">
-              <li><a href="#docs" className="hover:text-ibuild-red">Documentation</a></li>
+              <li><a href="/tools" className="hover:text-ibuild-red">Documentation</a></li>
               <li><a href="/support" className="hover:text-ibuild-red">Customer Support</a></li>
             </ul>
           </div>

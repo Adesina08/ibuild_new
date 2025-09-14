fec174124d4e14b0eb3b39f6e5a65918e834f7a4
Adesina08 <153903110+Adesina08@users.noreply.github.com>
2025-09-14 20:32:23 +0100

    feat: make images responsive and lazy load

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,26 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+function applyLazyLoading(img: HTMLImageElement) {
+  if (!img.hasAttribute("loading")) {
+    img.setAttribute("loading", "lazy");
+  }
+}
+
+document.querySelectorAll("img").forEach((img) => applyLazyLoading(img));
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      if (node instanceof HTMLImageElement) {
+        applyLazyLoading(node);
+      } else if (node instanceof HTMLElement) {
+        node.querySelectorAll("img").forEach((img) => applyLazyLoading(img));
+      }
+    });
+  });
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
 createRoot(document.getElementById("root")!).render(<App />);
